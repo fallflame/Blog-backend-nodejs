f791313405d09593cee5e0ae8d495a0ca3be87e4
Yan Xu <xuyantc@msn.com>
2014-12-25 07:54:53 -0500

    seperate front-end

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,4 @@ var path = require('path');
 var viewPath = path.resolve(__dirname, '../views/');
 
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  //res.render('index', { title: 'Express' });
-	res.sendFile(viewPath + '/index.html');
-
-});
-
-
 module.exports = router;
